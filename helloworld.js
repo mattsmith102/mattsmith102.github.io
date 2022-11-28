a3b54fc6bad240f7e6c7db5515ac7f06e6ffa631
Matt Smith <hello@mattsmith.tv>
2022-11-28 21:14:36 +0000

    Initial commit

---
+++ helloworld.js
@@ -0,0 +1,37 @@
+{
+  "name": "Hello World",
+  "description": "My first widget.json.",
+  "data": {
+    "content_url": "https://wd.gt/"
+  },
+  "layouts": {
+    "hello_small": {
+      "size": "small",
+      "styles": {
+        "colors": {
+          "mig_red": {
+            "color": "#DA2127"
+          }
+        }
+      },
+      "layers": [
+        {
+          "rows": [
+            {
+              "height": 12,
+              "cells": [
+                {
+                  "width": 12,
+                  "background_color_style": "mig_red",
+                  "text": {
+                    "string": "This is Cool!"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
